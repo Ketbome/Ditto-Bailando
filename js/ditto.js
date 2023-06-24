f325dc9e9425c850c4551c661f8a384802138f65
Ketbome <pims.2711@gmail.com>
2023-06-23 20:45:40 -0400

    fix: :bug: Optimización de codigo y agregado de clicks

--- js/ditto.js
+++ js/ditto.js
@@ -14,89 +14,39 @@ document.addEventListener('DOMContentLoaded', function() {
     audio.play();
     generateRandomDittos();
     setInterval(addCornerImage, 10000);
+    clickme();
   });
 });
 
 function generateRandomDittos() {
-  var numDittos = 14; // Número de ditos aleatorios a generar en cada dirección
+  var numDittos = 5; // Número de ditos aleatorios
   var dittoContainer = document.getElementById('dance');
-  var isRightDirection = true;
 
   for (var i = 0; i < numDittos; i++) {
-    var randomSize = Math.floor(Math.random() * 30) + 5; // Tamaño aleatorio entre 50 y 300 pixels
-    var randomSpeed = Math.floor(Math.random() * 15) + 7; // Velocidad aleatoria entre 5 y 20 segundos
-    var top = Math.floor(Math.random() * 70); // Posición aleatoria entre 0 y 70%
-
-    if (i % 2 == 0){
-      var randomDittoRight = createRandomDittoright(randomSize, randomSpeed, top);
-      dittoContainer.appendChild(randomDittoRight);
-    }else{
-      var randomDittoLeft = createRandomDittoleft(randomSize, randomSpeed, top);
-      dittoContainer.appendChild(randomDittoLeft);
-    }
-
+    var randomSize = Math.floor(Math.random() * 30) + 5; // Tamaño aleatorio
+    var randomSpeed = Math.floor(Math.random() * 15) + 7; // Velocidad aleatoria
+    var top = Math.floor(Math.random() * 70); // Posición aleatoria
+    var randomDitto = createRandomDitto(randomSize, randomSpeed, top, i);
     
-    isRightDirection = !isRightDirection;
+    dittoContainer.appendChild(randomDitto);
   }
 
 }
 
-function createRandomDittoright(size, speed, top) {
+function createRandomDitto(size, speed, top, i) {
   var randomDitto = document.createElement('img');
   randomDitto.src = 'img/ditto.gif';
   randomDitto.alt = 'Random Ditto';
-  randomDitto.classList.add('random-ditto-right');
   randomDitto.style.height = size + '%';
   randomDitto.style.animationDuration = speed + 's';
   randomDitto.style.top = top + '%';
-
-  randomDitto.addEventListener('animationend', function() {
-    dittoContainer.removeChild(this);
-    generateNewDittoright();
-  });
-
-  return randomDitto;
-}
-
-function createRandomDittoleft(size, speed, top) {
-  var randomDitto = document.createElement('img');
-  randomDitto.src = 'img/ditto.gif';
-  randomDitto.alt = 'Random Ditto';
-  randomDitto.classList.add('random-ditto-left');
-  randomDitto.style.height = size + '%';
-  randomDitto.style.animationDuration = speed + 's';
-  randomDitto.style.top = top + '%';
-
-  randomDitto.addEventListener('animationend', function() {
-    dittoContainer.removeChild(this);
-    generateNewDittoleft();
-  });
+  if( i % 2 == 0){
+    randomDitto.classList.add('random-ditto-right');
+  }else{
+    randomDitto.classList.add('random-ditto-left');
+  }
 
   return randomDitto;
-}
-
-function generateNewDittoright() {
-  var dittoContainer = document.getElementById('dance');
-
-  var randomSize = Math.floor(Math.random() * 250) + 50; // Tamaño aleatorio entre 50 y 300 pixels
-  var randomSpeed = Math.floor(Math.random() * 15) + 5; // Velocidad aleatoria entre 5 y 20 segundos
-  var top = Math.floor(Math.random() * 70); // Posición aleatoria entre 0 y 70%
-
-  var randomDitto = createRandomDitto(randomSize, randomSpeed, top);
-
-  dittoContainer.appendChild(randomDitto);
-}
-
-function generateNewDittoleft() {
-  var dittoContainer = document.getElementById('dance');
-
-  var randomSize = Math.floor(Math.random() * 30) + 5; // Tamaño aleatorio entre 50 y 300 pixels
-  var randomSpeed = Math.floor(Math.random() * 8) + 5; // Velocidad aleatoria entre 5 y 20 segundos
-  var top = Math.floor(Math.random() * 70); // Posición aleatoria entre 0 y 70%
-
-  var randomDitto = createRandomDittoleft(randomSize, randomSpeed, top);
-
-  dittoContainer.appendChild(randomDitto);
 }
 
 function addCornerImage() {
@@ -117,6 +67,39 @@ function addCornerImage() {
   // Crear la imagen
   var cornerImage = document.createElement('img');
   cornerImage.src = selectedImagePath; // Reemplaza 'ruta-de-la-imagen' con la ruta real de la imagen
+  cornerImage.classList.add('corner-image');
+
+  // Agregar la imagen al contenedor
+  cornerImageContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar la imagen
+  cornerImageContainer.appendChild(cornerImage);
+
+  // Establecer un temporizador para eliminar la imagen después de 5 segundos
+  setTimeout(function() {
+    cornerImageContainer.innerHTML = ''; // Eliminar la imagen del contenedor
+  }, 5000);
+}
+
+// Agregar el evento click para generar un nuevo Ditto
+document.addEventListener('click', function() {
+  generateNewDitto();
+});
+
+function generateNewDitto() {
+  var dittoContainer = document.getElementById('dance');
+
+  var randomSize = Math.floor(Math.random() * 30) + 5; // Tamaño aleatorio entre 50 y 300 pixels
+  var randomSpeed = Math.floor(Math.random() * 8) + 5; // Velocidad aleatoria entre 5 y 20 segundos
+  var top = Math.floor(Math.random() * 70); // Posición aleatoria entre 0 y 70%
+
+  var randomDitto = createRandomDitto(randomSize, randomSpeed, top);
+
+  dittoContainer.appendChild(randomDitto);
+}
+
+function clickme() {
+  // Crear la imagen
+  var cornerImage = document.createElement('img');
+  cornerImage.src = "img/click.gif"; // Reemplaza 'ruta-de-la-imagen' con la ruta real de la imagen
   cornerImage.classList.add('corner-image');
 
   // Agregar la imagen al contenedor
